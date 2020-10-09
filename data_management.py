c88c69f89e0c8e89a9c544cf931f7beda2c5eb5c
Claudia Bigoni <cla.bigoni@gmail.com>
2020-10-09 11:25:58 +0200

    Combine trials by sequence and fix error in pickle dump

--- data_management.py
+++ data_management.py
@@ -1,45 +1,64 @@
 """
 Author: Claudia Bigoni
 Date: 08.10.2020
+Date_latest_update: 09.10.2020
 Description: Divide trials into single pulse or double pulse for both EEG and EMG
 """
 import os
 import mne
 from scipy.io import loadmat
 import pickle
-import numpy as np
 
 from emg_class import EMG
 
 
+errors_found = []
 data_dir = 'C:\\Users\\bigoni\\Data\\TMS-EEG'
 subjects = [i for i in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, i))]
-
 for sub_id in subjects:
+    print(f'Subject {sub_id}')
     t_points_eeg = [i for i in os.listdir(os.path.join(data_dir, sub_id, 'EEG')) if os.path.isdir(os.path.join(
         data_dir, sub_id, 'EEG', i))]
     t_points_emg = [i for i in os.listdir(os.path.join(data_dir, sub_id, 'EEG')) if os.path.isdir(os.path.join(
         data_dir, sub_id, 'EMG', i))]
     t_points = set(t_points_eeg + t_points_emg)
-
-    sub_emg_data_sp = EMG(sub_id, 'SP')
-    sub_emg_data_dp = EMG(sub_id, 'DP')
     for idx_tpoint, tpoint in enumerate(t_points):
         print(f'T point {tpoint}')
-        for seq in ['A1', 'B1', 'A2', 'B2', 'A3', 'B3']:
+        sub_emg_data_sp = EMG(sub_id, 'SP')
+        sub_emg_data_dp = EMG(sub_id, 'DP')
+        for idx_seq, seq in enumerate(['A1', 'B1', 'A2', 'B2', 'A3', 'B3']):
             print(f'sequence {seq}')
-            seq_epochs_emg = loadmat(os.path.join(data_dir, sub_id, 'EMG', tpoint, 'Cleaned Data', f'TiMeS_{sub_id}_{tpoint}_'
-                                                  f'{seq}_GUI_Results.mat'))
-            trial_pulses = seq_epochs_emg['States']
-            for idx_t, t in enumerate(trial_pulses):
-                print(f'trial n. {idx_t}')
-                if t == 1:
-                    sub_emg_data_sp.add_trial(seq_epochs_emg, idx_tpoint, idx_t)
-                elif t == 2:
-                    sub_emg_data_dp.add_trial(seq_epochs_emg, idx_tpoint, idx_t)
-        pickle.dump(sub_emg_data_sp, os.path.join(data_dir, sub_id, tpoint, 'EMG', 'EMG_data_SP.pkl'))
-        pickle.dump(sub_emg_data_dp, os.path.join(data_dir, sub_id, tpoint, 'EMG', 'EMG_data_DP.pkl'))
+            try:
+                seq_epochs_emg = loadmat(
+                    os.path.join(data_dir, sub_id, 'EMG', tpoint, 'Cleaned Data',
+                                 f'TiMeS_{sub_id}_{tpoint}_{seq}_GUI_Results.mat'))
+                trial_pulses = seq_epochs_emg['States']
+                sub_emg_data_sp.add_trials(seq_epochs_emg, trial_pulses, idx_seq)
+                sub_emg_data_sp.add_info_trials(seq_epochs_emg['SettingsGeneral'], len(trial_pulses[trial_pulses == 1]))
+                sub_emg_data_dp.add_trials(seq_epochs_emg, trial_pulses, idx_seq)
+                sub_emg_data_dp.add_info_trials(seq_epochs_emg['SettingsGeneral'], len(trial_pulses[trial_pulses == 2]))
+            except FileNotFoundError:
+                errors_found.append(f'EMG - {sub_id}-{tpoint}-{seq}')
+            # for idx_t, t in enumerate(trial_pulses):
+            #     print(f'trial n. {idx_t}')
+            #     if t == 1:
+            #         sub_emg_data_sp.add_trial(seq_epochs_emg, idx_tpoint, idx_t)
+            #     elif t == 2:
+            #         sub_emg_data_dp.add_trial(seq_epochs_emg, idx_tpoint, idx_t)
+        f_name = os.path.join(data_dir, sub_id, 'EMG', tpoint, 'EMG_data_SP.pkl')
+        p_file = open(f_name, 'wb')
+        pickle.dump(sub_emg_data_sp, p_file)
+        p_file.close()
+        f_name = os.path.join(data_dir, sub_id, 'EMG', tpoint, 'EMG_data_DP.pkl')
+        p_file = open(f_name, 'wb')
+        pickle.dump(sub_emg_data_dp, p_file)
+        p_file.close()
 
+    f_name = os.path.join(data_dir, sub_id, 'EMG', 'not_found_files')
+    p_file = open(f_name, 'w')
+    for item in errors_found:
+        p_file.write(item)
+    p_file.close()
     # sub_eeg_data = []
     # for pulse in ['SP', 'DP']:
     #     for b in blocks:
@@ -47,5 +66,3 @@ for sub_id in subjects:
     #                                                               f'_{b}_ROI.set'))
     #         data_epochs_eeg = block_eeg_epochs.get_data()     # trials x channels x samples
 
-    pickle.dump(sub_emg_data_sp, os.path.join(data_dir, sub_id, 'EMG_data_SP.pkl'))
-    pickle.dump(sub_emg_data_dp, os.path.join(data_dir, sub_id, 'EMG_data_DP.pkl'))
--- emg_class.py
+++ emg_class.py
@@ -1,3 +1,9 @@
+"""
+Author: Claudia Bigoni
+Date: 08.10.2020
+Date_latest_update: 09.10.2020
+Description: Create structure for EMG data following .mat files
+"""
 import numpy as np
 
 
@@ -11,14 +17,14 @@ class SettingsGeneral:
         self.signal_flipping = []
         self.graph_window_width = []
 
-    def add_data(self, settings):
-        self.name_examiner.append(settings['NameOfExaminer'][0][0][0])
-        self.channel_muscles.append(settings['ChannelMuscles'][0][0])
-        self.s_freq.append(settings['SamplFreq'][0][0][0][0])
-        self.transmission_delay.append(settings['TransmissionDelay'][0][0][0][0])
-        self.tms_trigger_time.append(settings['TMSTriggerTime'][0][0][0][0])
-        self.signal_flipping.append(settings['SignalFlippingOn'][0][0][0][0])
-        self.graph_window_width.append(settings['GraphWindowWidth'][0][0][0][0])
+    def add_data(self, settings, no_trials):
+        self.name_examiner.append([settings['NameOfExaminer'][0][0][0]] * no_trials)
+        self.channel_muscles.append([settings['ChannelMuscles'][0][0]] * no_trials)
+        self.s_freq.append([settings['SamplFreq'][0][0][0][0]] * no_trials)
+        self.transmission_delay.append([settings['TransmissionDelay'][0][0][0][0]] * no_trials)
+        self.tms_trigger_time.append([settings['TMSTriggerTime'][0][0][0][0]] * no_trials)
+        self.signal_flipping.append([settings['SignalFlippingOn'][0][0][0][0]] * no_trials)
+        self.graph_window_width.append([settings['GraphWindowWidth'][0][0][0][0]] * no_trials)
 
 
 class SettingsMeasurement:
@@ -35,18 +41,18 @@ class SettingsMeasurement:
         self.low_pass_filt = []
         self.notch_filt = []
 
-    def add_data(self, settings):
-        self.dc_offset_start_time.append(settings['DCOffsetStartTime'][0][0][0][0])
-        self.dc_offset_stop_time.append(settings['DCOffsetStopTime'][0][0][0][0])
-        self.emg_background_Start_time.append(settings['EMGbackgroundStartTime'][0][0][0][0])
-        self.emg_latency_sd.append(settings['EMGLatencySDvalue'][0][0][0][0])
-        self.emg_module_offset.append(settings['EMGmoduleOffset'][0][0][0][0])
-        self.cutoff_freq_high.append(settings['cutOffFreqHigh'][0][0][0][0])
-        self.cutoff_freq_low.append(settings['cutOffFreqLow'][0][0][0][0])
-        self.notch_freq.append(settings['notchFreq'][0][0][0][0])
-        self.high_pass_filt.append(settings['highPassOn'][0][0][0][0])
-        self.low_pass_filt.append(settings['lowPassOn'][0][0][0][0])
-        self.notch_filt.append(settings['notchOn'][0][0][0][0])
+    def add_data(self, settings, trials, pulse):
+        self.dc_offset_start_time.append(settings['DCOffsetStartTime'][0][0][trials[trials == pulse]])
+        self.dc_offset_stop_time.append(settings['DCOffsetStopTime'][0][0][trials[trials == pulse]])
+        self.emg_background_Start_time.append(settings['EMGbackgroundStartTime'][0][0][trials[trials == pulse]])
+        self.emg_latency_sd.append(settings['EMGLatencySDvalue'][0][0][trials[trials == pulse]])
+        self.emg_module_offset.append(settings['EMGmoduleOffset'][0][0][trials[trials == pulse]])
+        self.cutoff_freq_high.append(settings['cutOffFreqHigh'][0][0][trials[trials == pulse]])
+        self.cutoff_freq_low.append(settings['cutOffFreqLow'][0][0][trials[trials == pulse]])
+        self.notch_freq.append(settings['notchFreq'][0][0][trials[trials == pulse]])
+        self.high_pass_filt.append(settings['highPassOn'][0][0][trials[trials == pulse]])
+        self.low_pass_filt.append(settings['lowPassOn'][0][0][trials[trials == pulse]])
+        self.notch_filt.append(settings['notchOn'][0][0][trials[trials == pulse]])
 
 
 class SettingsRejection:
@@ -80,36 +86,42 @@ class SettingsRejection:
         self.baseline_bef_tms_threshold_per_trial_multipl = []
         self.baseline_threshold_per_trial_multipl = []
 
-    def add_data(self, settings):
-        self.baseline_warning.append(settings['BaselineWarningOn'][0][0][0][0])
-        self.baseline_bef_tms_warning.append(settings['BaselineBefTMSWarningOn'][0][0][0][0])
-        self.mep_noise_check.append(settings['MEPNoiseCheckOn'][0][0][0][0])
-        self.emg_noise_check_after_mep.append(settings['EMGNoiseCheckAfterMEP'][0][0][0][0])
-        self.mep_vpp_check.append(settings['MEPVppCheckOn'][0][0][0][0])
-        self.baseline_threshold.append(settings['BaselineThreshold'][0][0][0][0])
-        self.baseline_correction_start.append(settings['baselineCorrectionStart'][0][0][0][0])
-        self.baseline_correction_stop.append(settings['baselineCorrectionStop'][0][0][0][0])
-        self.baseline_bef_tms_threshold.append(settings['BaselineBefTMSthreshold'][0][0][0][0])
-        self.baseline_bef_tms_start.append(settings['BaselineBefTMSstart'][0][0][0][0])
-        self.baseline_bef_tms_stop.append(settings['BaselineBefTMSstop'][0][0][0][0])
-        self.mep_noise_threshold.append(settings['MEPnoiseThreshold'][0][0][0][0])
-        self.mep_noise_checktime_offset_start.append(settings['MEPnoiseCheckTimeOffsetStart'][0][0][0][0])
-        self.mep_noise_checktime_offset_stop.append(settings['MEPnoiseCheckTimeOffsetStop'][0][0][0][0])
-        self.emg_after_mep_noise_threshold.append(settings['EMGafterMEPnoisethreshold'][0][0][0][0])
-        self.emg_after_mep_noise_time_offset_start.append(settings['EMGNoiseAferMEPtimeOffsetStart'][0][0][0][0])
-        self.emg_after_mep_noise_time_offset_stop.append(settings['EMGNoiseAferMEPtimeOffsetStop'][0][0][0][0])
-        self.mep_pp_threshold.append(settings['MEPVppThreshold'][0][0][0][0])
-        self.mep_checktime_offset_start.append(settings['MEPcheckTimeOffsetStart'][0][0])
-        self.mep_checktime_offset_stop.append(settings['MEPchecktTimeOffsetStop'][0][0])
-        self.important_ch_for_reject.append(settings['ImportantChForReject'][0][0][0][0])
-        self.std_baseline_value.append(settings['stdBaselineValue'][0][0])
-        self.threshold_type.append(settings['ThresholdType'][0][0][0])
-        self.mep_threshold_per_trial_multipl.append(settings['MEPVppThresholdPerTrialMultipl'][0][0])
-        self.mep_noise_threshold_per_trial_multipl.append(settings['MEPnoisethresholdPerTrialMultipl'][0][0])
+    def add_data(self, settings, trials, pulse):
+        self.baseline_warning.append(settings['BaselineWarningOn'][0][0][trials[trials == pulse]])
+        self.baseline_bef_tms_warning.append(settings['BaselineBefTMSWarningOn'][0][0][trials[trials == pulse]])
+        self.mep_noise_check.append(settings['MEPNoiseCheckOn'][0][0][trials[trials == pulse]])
+        self.emg_noise_check_after_mep.append(settings['EMGNoiseCheckAfterMEP'][0][0][trials[trials == pulse]])
+        self.mep_vpp_check.append(settings['MEPVppCheckOn'][0][0][trials[trials == pulse]])
+        self.baseline_threshold.append(settings['BaselineThreshold'][0][0][trials[trials == pulse]])
+        self.baseline_correction_start.append(settings['baselineCorrectionStart'][0][0][trials[trials == pulse]])
+        self.baseline_correction_stop.append(settings['baselineCorrectionStop'][0][0][trials[trials == pulse]])
+        self.baseline_bef_tms_threshold.append(settings['BaselineBefTMSthreshold'][0][0][trials[trials == pulse]])
+        self.baseline_bef_tms_start.append(settings['BaselineBefTMSstart'][0][0][trials[trials == pulse]])
+        self.baseline_bef_tms_stop.append(settings['BaselineBefTMSstop'][0][0][trials[trials == pulse]])
+        self.mep_noise_threshold.append(settings['MEPnoiseThreshold'][0][0][trials[trials == pulse]])
+        self.mep_noise_checktime_offset_start.append(settings['MEPnoiseCheckTimeOffsetStart'][0][0][trials[trials == pulse]])
+        self.mep_noise_checktime_offset_stop.append(settings['MEPnoiseCheckTimeOffsetStop'][0][0][trials[trials == pulse]])
+        self.emg_after_mep_noise_threshold.append(settings['EMGafterMEPnoisethreshold'][0][0][trials[trials == pulse]])
+        self.emg_after_mep_noise_time_offset_start.append(
+            settings['EMGNoiseAferMEPtimeOffsetStart'][0][0][trials[trials == pulse]])
+        self.emg_after_mep_noise_time_offset_stop.append(
+            settings['EMGNoiseAferMEPtimeOffsetStop'][0][0][trials[trials == pulse]])
+        self.mep_pp_threshold.append(settings['MEPVppThreshold'][0][0][trials[trials == pulse]])
+        self.mep_checktime_offset_start.append(settings['MEPcheckTimeOffsetStart'][0][0][:, trials[trials == pulse]])
+        self.mep_checktime_offset_stop.append(settings['MEPchecktTimeOffsetStop'][0][0][:, trials[trials == pulse]])
+        self.important_ch_for_reject.append(settings['ImportantChForReject'][0][0][trials[trials == pulse]])
+        self.std_baseline_value.append(settings['stdBaselineValue'][0][0][trials[trials == pulse], :])
+        self.threshold_type.append([settings['ThresholdType'][0][0][0]]*len(trials[trials == pulse]))
+        self.mep_threshold_per_trial_multipl.append(
+            settings['MEPVppThresholdPerTrialMultipl'][0][0][trials[trials == pulse], :])
+        self.mep_noise_threshold_per_trial_multipl.append(
+            settings['MEPnoisethresholdPerTrialMultipl'][0][0][trials[trials == pulse], :])
         self.emg_after_mep_noise_threshold_per_trial_multipl.append(
-            settings['EMGafterMEPnoisethresholdPerTrialMultipl'][0][0])
-        self.baseline_bef_tms_threshold_per_trial_multipl.append(settings['BaselineBefTMSthresholdPerTrialMultipl'][0][0])
-        self.baseline_threshold_per_trial_multipl.append(settings['BaselineThresholdPerTrialMultipl'][0][0])
+            settings['EMGafterMEPnoisethresholdPerTrialMultipl'][0][0][trials[trials == pulse], :])
+        self.baseline_bef_tms_threshold_per_trial_multipl.append(
+            settings['BaselineBefTMSthresholdPerTrialMultipl'][0][0][trials[trials == pulse], :])
+        self.baseline_threshold_per_trial_multipl.append(
+            settings['BaselineThresholdPerTrialMultipl'][0][0][trials[trials == pulse], :])
 
 
 class ResultsRejection:
@@ -131,30 +143,37 @@ class ResultsRejection:
         self.C5_noise_after_mep_value = []
         self.C5_noise_after_mep_decision = []
 
-    def add_data(self, res):
-        self.discard_criteria.append(res['discardCriteria'][0][0][0][0])
-        self.channels_for_rejection.append(res['channelsForRejection'][0][0][0][0])
-        self.rej_results_per_ch_chosen_criteria.append(res['rejectionResultsPerChannelChosenCriteria'][0][0])
-        self.rej_results_per_ch_all_criteria.append(res['rejectionResultsPerChannelAllCriteria'][0][0])
-        self.rej_final_auto.append(res['rejectFinalAuto'][0][0][0][0])
-        self.rej_final_manual.append(res['rejectFinalManual'][0][0][0][0])
-        self.C1_baseline_value.append(res['C1_baseline_values'][0][0])
-        self.C1_baseline_decision.append(res['C1_baseline_decision'][0][0])
-        self.C2_baseline_bef_tms_value.append(res['C2_baselineBefTMS_values'][0][0])
-        self.C2_baseline_bef_tms_decision.append(res['C2_baselineBefTMS_decision'][0][0])
-        self.C3_mep_amp_value.append(res['C3_MEPamp_values'][0][0])
-        self.C3_mep_amp_decision.append(res['C3_MEPamp_decision'][0][0])
-        self.C4_noise_bef_mep_value.append(res['C4_noiseBefMEP_values'][0][0])
-        self.C4_noise_bef_mep_decision.append(res['C4_noiseBefMEP_decision'][0][0])
-        self.C5_noise_after_mep_value.append(res['C5_noiseAfterMEP_values'][0][0])
-        self.C5_noise_after_mep_decision.append(res['C5_noiseAfterMEP_decision'][0][0])
+    def add_data(self, res, trials, pulse):
+        self.discard_criteria.append(res['discardCriteria'][0][0][trials[trials == pulse]])
+        self.channels_for_rejection.append(res['channelsForRejection'][0][0][trials[trials == pulse]])
+        self.rej_results_per_ch_chosen_criteria.append(
+            res['rejectionResultsPerChannelChosenCriteria'][0][0][trials[trials == pulse], :])
+        self.rej_results_per_ch_all_criteria.append(
+            res['rejectionResultsPerChannelAllCriteria'][0][0][trials[trials == pulse], :])
+        self.rej_final_auto.append(res['rejectFinalAuto'][0][0][trials[trials == pulse]])
+        self.rej_final_manual.append(res['rejectFinalManual'][0][0][trials[trials == pulse]])
+        self.C1_baseline_value.append(res['C1_baseline_values'][0][0][trials[trials == pulse], :])
+        self.C1_baseline_decision.append(res['C1_baseline_decision'][0][0][trials[trials == pulse], :])
+        self.C2_baseline_bef_tms_value.append(res['C2_baselineBefTMS_values'][0][0][trials[trials == pulse], :])
+        self.C2_baseline_bef_tms_decision.append(res['C2_baselineBefTMS_decision'][0][0][trials[trials == pulse], :])
+        self.C3_mep_amp_value.append(res['C3_MEPamp_values'][0][0][trials[trials == pulse], :])
+        self.C3_mep_amp_decision.append(res['C3_MEPamp_decision'][0][0][trials[trials == pulse], :])
+        self.C4_noise_bef_mep_value.append(res['C4_noiseBefMEP_values'][0][0][trials[trials == pulse], :])
+        self.C4_noise_bef_mep_decision.append(res['C4_noiseBefMEP_decision'][0][0][trials[trials == pulse], :])
+        self.C5_noise_after_mep_value.append(res['C5_noiseAfterMEP_values'][0][0][trials[trials == pulse], :])
+        self.C5_noise_after_mep_decision.append(res['C5_noiseAfterMEP_decision'][0][0][trials[trials == pulse], :])
 
 
 class EMG:
     def __init__(self, sub_id, pulse='SP'):
         self.sub_id = sub_id
         self.pulse = pulse
+        if self.pulse == 'SP':
+            self.pulse_no = 1
+        elif self.pulse == 'DP':
+            self.pulse_no = 2
         self.trial = 0
+        self.seq = 0
         self.trials = []
         self.block = []
         self.trials_all = []
@@ -179,36 +198,69 @@ class EMG:
         self.emg_latency_values = []
         self.std_baseline_values = []
 
-    def set_emg_data(self, data):
-        self.mep = data['MEPVppValues'][0][0]
-        self.emg_module_values = data['EMGModuleValues'][0][0]
-        self.emg_rms_values = data['EMGRmsValues'][0][0]
-        self.emg_latency_values = data['EMGLatencyValues'][0][0]
-        self.std_baseline_values = data['stdBaselineValue'][0][0]
+    def set_emg_data(self, data, trials):
+        self.mep = data['MEPVppValues'][0][0][trials[trials == self.pulse_no], :]
+        self.emg_module_values = data['EMGModuleValues'][0][0][trials[trials == self.pulse_no], :]
+        self.emg_rms_values = data['EMGRmsValues'][0][0][trials[trials == self.pulse_no], :]
+        self.emg_latency_values = data['EMGLatencyValues'][0][0][trials[trials == self.pulse_no], :]
+        self.std_baseline_values = data['stdBaselineValue'][0][0][trials[trials == self.pulse_no], :]
 
-    def add_emg_data(self, data):
-        self.mep = np.concatenate((self.mep, data['MEPVppValues'][0][0]), axis=0)
-        self.emg_module_values = np.concatenate((self.emg_module_values, data['EMGModuleValues'][0][0]), axis=0)
-        self.emg_rms_values = np.concatenate((self.emg_module_values, data['EMGRmsValues'][0][0]), axis=0)
-        self.emg_latency_values = np.concatenate((self.emg_module_values, data['EMGLatencyValues'][0][0]), axis=0)
-        self.std_baseline_values = np.concatenate((self.emg_module_values, data['stdBaselineValue'][0][0]), axis=0)
+    def add_emg_data(self, data, trials):
+        self.mep = np.concatenate((self.mep, data['MEPVppValues'][0][0][trials[trials == self.pulse_no], :]), axis=0)
+        self.emg_module_values = np.concatenate(
+            (self.emg_module_values, data['EMGModuleValues'][0][0][trials[trials == self.pulse_no], :]), axis=0)
+        self.emg_rms_values = np.concatenate(
+            (self.emg_module_values, data['EMGRmsValues'][0][0][trials[trials == self.pulse_no], :]), axis=0)
+        self.emg_latency_values = np.concatenate(
+            (self.emg_module_values, data['EMGLatencyValues'][0][0][trials[trials == self.pulse_no], :]), axis=0)
+        self.std_baseline_values = np.concatenate(
+            (self.emg_module_values, data['stdBaselineValue'][0][0][trials[trials == self.pulse_no], :]), axis=0)
 
-    def add_trial(self, epochs, block_no, trial_no):
-        self.settings.add_data(epochs['SettingsGeneral'])
-        self.settings_measurement.add_data(epochs['SettingsMeasurement'])
-        self.settings_rejection.add_data(epochs['SettingsRejection'])
-        self.rej_results.add_data(epochs['RejectionResults'])
+    # def add_trial(self, epochs, block_no, trial_no):
+    #     # self.settings.add_data(epochs['SettingsGeneral'])
+    #     # self.settings_measurement.add_data(epochs['SettingsMeasurement'])
+    #     # self.settings_rejection.add_data(epochs['SettingsRejection'])
+    #     # self.rej_results.add_data(epochs['RejectionResults'])
+    #
+    #     if self.trial == 0:
+    #         self.processed_data = epochs['AllProcessedDat']     # samples x ch x trials
+    #         self.original_data = epochs['AllOriginalDat']     # samples x ch x trials
+    #         self.set_emg_data(epochs['EMGdata'])
+    #     else:
+    #         self.processed_data = np.concatenate((self.processed_data, epochs['AllProcessedDat']), axis=2)
+    #         self.original_data = np.concatenate((self.original_data, epochs['AllOriginalDat']), axis=2)
+    #         self.add_emg_data(epochs['EMGdata'])
+    #
+    #     self.trial += 1
+    #     self.trials.append(trial_no)
+    #     self.block.append(block_no)
+    #     self.trials_all.append(trial_no + 60*block_no)
 
-        if self.trial == 0:
-            self.processed_data = epochs['AllProcessedDat']     # samples x ch x trials
-            self.processed_data = epochs['AllOriginalDat']     # samples x ch x trials
-            self.set_emg_data(epochs['EMGdata'])
+    def add_trials(self, epochs_emg, trials, block_no):
+        if self.seq == 0:
+            self.processed_data = epochs_emg['AllProcessedDat'][:, :, trials[trials == self.pulse_no]]
+            # samples x ch x  trials
+            self.original_data = epochs_emg['AllOriginalDat'][:, :, trials[trials == self.pulse_no]]
+            # samples x ch x trials
+            self.set_emg_data(epochs_emg['EMGdata'], trials)
+            self.settings_measurement.add_data(epochs_emg['SettingsMeasurement'], trials, self.pulse)
+            self.settings_rejection.add_data(epochs_emg['SettingsRejection'], trials, self.pulse_no)
+            self.rej_results.add_data(epochs_emg['RejectionResults'], trials, self.pulse_no)
         else:
-            self.processed_data = np.concatenate((self.processed_data, epochs['AllProcessedDat']), axis=2)
-            self.processed_data = np.concatenate((self.processed_data, epochs['AllOriginalDat']), axis=2)
-            self.add_emg_data(epochs['EMGdata'])
+            self.processed_data = np.concatenate(
+                (self.processed_data, epochs_emg['AllProcessedDat'][:, :, trials[trials == self.pulse_no]]), axis=2)
+            self.original_data = np.concatenate(
+                (self.original_data, epochs_emg['AllOriginalDat'][:, :, trials[trials == self.pulse_no]]), axis=2)
+            self.add_emg_data(epochs_emg['EMGdata'], trials)
+            self.settings_measurement.add_data(epochs_emg['SettingsMeasurement'], trials, self.pulse)
+            self.settings_rejection.add_data(epochs_emg['SettingsRejection'], trials, self.pulse_no)
+            self.rej_results.add_data(epochs_emg['RejectionResults'], trials, self.pulse_no)
 
-        self.trial += 1
+        self.seq += 1
+        trial_no = trials[trials == self.pulse_no]
         self.trials.append(trial_no)
         self.block.append(block_no)
         self.trials_all.append(trial_no + 60*block_no)
+
+    def add_info_trials(self, settings_general, no_trials):
+        self.settings.add_data(settings_general, no_trials)
